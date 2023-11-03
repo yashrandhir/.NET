01364690fc5865d1f7ffeb199925d9dd0cc2d5a7
Yash Randhir <132140698+yashrandhir@users.noreply.github.com>
2023-11-04 00:42:26 +0530

    Day3.cs

---
+++ Day3.cs
@@ -0,0 +1,42 @@
+using System;
+namespace Demo
+{
+
+  public class Student
+    {
+        string Name;
+        int Maths;
+        int Science;
+        int Eng;
+        int Marks;
+
+        public Student(string name, int maths, int science, int eng)
+        {
+          this.Name = name;
+           this.Maths = maths;
+          this.Science = science;
+          this.Eng = eng;
+           
+        }
+        public void Totalmarks()
+        {
+            Marks = Maths + Science + Eng;
+        }
+        public string Display()
+        {
+           Totalmarks();
+            return String.Format("Name = {0} Total ={1} out of 300", Name, Marks);
+        }
+    }
+    public class Yash
+    {
+        public static void Main()
+        {
+            Student student1 = new Student("Bapu",50,60,98);
+            Student student2 = new Student("appa",90, 70, 60);
+            Console.WriteLine("Info about Student1 is" + student1.Display());
+            Console.WriteLine("Info about Student2 is"+ student2.Display());
+        }
+    }
+
+}
